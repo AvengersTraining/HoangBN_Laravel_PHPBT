24d1f37e71d4323c81022cdee9157c9fd059f2fb
hoangbn-1772 <48110404+hoangbn-1772@users.noreply.github.com>
2020-07-03 08:30:01 +0700

    Task#blog-migration: Make migration (#2)
    * Task#blog-migration: Make migration
    
    Co-authored-by: bui.ngoc.hoang <SUN-ASTERISK\bui.ngoc.hoang@b121772-pc.sun-asterisk.com>

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('id');
+            $table->string('full_name', 100);
+            $table->string('display_name', 100)->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone_number', 10);
+            $table->string('address', 255)->nullable();
+            $table->string('email', 100)->unique();
+            $table->string('password', 100);
+            $table->tinyInteger('is_admin')->default(0);
+            $table->string('avatar', 255)->nullable();
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
