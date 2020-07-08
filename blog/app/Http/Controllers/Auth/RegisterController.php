7438d228aa4f29e75c5ab91b8ae2c87c6b09c352
bui.ngoc.hoang <SUN-ASTERISK\bui.ngoc.hoang@b121772-pc.sun-asterisk.com>
2020-07-08 17:01:52 +0700

    Task#blog-register: Register, login, logout

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'regex:/^[0-9]{10}$/'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,7 +65,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'full_name' => $data['name'],
+            'phone_number' => $data['phone'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
